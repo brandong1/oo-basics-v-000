d4c9ea4521b1dd98782076459003ce5688f98b65
Brandon Green <brandon@brandonleeproductions.com>
2018-06-13 03:09:38 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -10,34 +10,36 @@ class Book
   def title
     @title
   end
-
-  def author=(author) #setter for author
-    @author = author
-  end
-  
-  def author #getter for author 
-    @author
-  end
-  
-  def page_count=(num)
-    @page_count = num
-  end
-  
-  def page_count
-    @page_count
-  end
-
-  def genre=(genre)
-    @genre = genre
-  end
-  
-  def genre
-    @genre
-  end
   
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
   
 end
+
+#  def author=(author) #setter for author
+#    @author = author
+#  end
+#  
+#  def author #getter for author 
+#    @author
+#  end
+#  
+#  def page_count=(num)
+#    @page_count = num
+#  end
+#  
+#  def page_count
+#    @page_count
+#  end
+#
+# def genre=(genre)
+#    @genre = genre
+#  end
+#  
+#  def genre
+#    @genre
+#  end
+#  
+
 
